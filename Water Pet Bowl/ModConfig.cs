f2f29a5f5e1b1a9292ed0c46c123c5c97264a9d8
ZeroXPatch <151591078+ZeroXPatch@users.noreply.github.com>
2025-12-27 17:07:17 -0600

    publish

--- Water Pet Bowl/ModConfig.cs
+++ Water Pet Bowl/ModConfig.cs
@@ -1,15 +1,7 @@
 ﻿namespace AutoFillPetBowl;
 
-public sealed class ModConfig
+public class ModConfig
 {
     public bool Enabled { get; set; } = true;
-
-    public bool OnlyFillIfEmpty { get; set; } = true;
-
-    /// <summary>Best-effort duration if the bowl uses a "days remaining" field.</summary>
-    public int FillDurationDays { get; set; } = 1;
-
-    public bool ShowHudMessage { get; set; } = false;
-
-    public bool DebugLogging { get; set; } = false;
+    public bool EnableLogging { get; set; } = false;
 }
